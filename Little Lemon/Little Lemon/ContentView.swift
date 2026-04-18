7b2e6d5615b43c3645fd8b6c610c90ab73086153
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2026-04-18 17:15:26 +0700

    Setup SwiftUI project

---
+++ Little Lemon/Little Lemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by Thwin Htoo Aung on 18/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Thwin Htoo Aung on 18/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
